2bb177c6ca780d96ddcce8092dd4ea73479022f7
Anshuman Verma <anshu.av97@gmail.com>
2019-01-08 10:55:30 +0530

    init maybe :confused:

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt.js');
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+
+const mongoose = require('./db/mongoose');
+const userRoutes = require('./routes/api/users');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+
+app.use('/api/users', userRoutes);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
