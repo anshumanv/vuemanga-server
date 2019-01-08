3d3a970d53dc1894dd1dd8466b6e1b0aaba6839c
Anshuman Verma <anshu.av97@gmail.com>
2019-01-08 12:41:40 +0530

    Complete user model

--- models/User.js
+++ models/User.js
@@ -16,6 +16,30 @@ const UserSchema = new Schema({
   password: {
     type: String,
     required: true
+  },
+  username: {
+    type: String,
+    trim: true
   }
 });
 
+// Pre save hook on User model
+UserSchema.pre('save', function(next) {
+  let user = this;
+
+  if(user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if(err) throw err;
+        user.password = hash;
+        next();
+      })
+    })
+  } else {
+    next();
+  }
+})
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
